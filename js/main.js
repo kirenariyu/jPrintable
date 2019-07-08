97ea5211c429ad39f20327a274c0b7df20d824d7
ytakuma <ytakuma@ezlifesoftwaresolutions.com>
2019-07-08 11:22:45 +0800

    Added dependencies

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/***************************************************************
+ * 
+ * Copyright (C) Yumi Takuma - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary
+ * Written by Yumi Takuma <yumitakuma@gmail.com>, July 2019
+ * 
+ ***************************************************************/
+
