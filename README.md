a9dddf8d933cbcd245a3cd9f06beb0e09727fb6b
ytakuma <ytakuma@ezlifesoftwaresolutions.com>
2020-05-26 11:44:00 +0800

    Updated to jTrint

--- README.md
+++ README.md
@@ -1,44 +1,151 @@
-# Print Responsively (Unfinished)
+# jTrint
 
-Print Responsively is a JavaScript plugin and it's under construction.
-
-This should address @media print complications by removing the default header and footer of browsers and automatically create a header and footer for you.
+jTrint is a jQuery plugin for printing documents of tables like payslip.
 
 ## Getting Started
 
-You'll be only needing the two files inside the print_responsively folder.
-
-* print-responsively.css
-* print-responsively.js
+* jtrint.js
 
 ### Prerequisites
 
-There are no prerequisites for this plugin.
+* jQuery
 
 ### Installing
 
-Just copy the two peint-responsive files (css and js) to your project. You may add the css file manually or dyamically using the plugin.
-
 ```
-printResponsively({
-    cssPath: 'path/to/print-responsively.css'
-});
+jTrint(options);
 ```
 
-## Usage
+## Options
 
-* [Usage Documentation](https://github.com/yumicchi/Print-Responsively/blob/master/USAGE.md)
+<table width="100%">
+    <tr>
+        <th align="left" colspan="5"><a href="#prerequisite-classes" name="prerequisite-classes">Prerequisite Classes</a></th>
+    </tr>
+	<tr>
+		<th align="left" colspan="1">Class</th>
+		<th align="left" colspan="2">Description</th>
+		<th align="left" colspan="2">Prerequisite</th>
+	</tr>
+    <tr>
+        <td><code>.hide-print</code></td>
+        <td colspan="2">This will hide the visible element on print.</td>
+        <td colspan="2"></td>
+    </tr>
+    <tr>
+        <td><code>.show-print</code></td>
+        <td colspan="2">This will show the hidden element on print.</td>
+        <td colspan="2"></td>
+    </tr>
+    <tr>
+        <td><code>.header-area</code></td>
+        <td colspan="2">Place this class in the parent container of the header where its associated contents are also included. You may combine this class with <code>.footer-area</code> class if there's also a footer incorporated.</td>
+        <td colspan="2"><code>header</code></td>
+    </tr>
+    <tr>
+        <td><code>.footer-area</code></td>
+        <td colspan="2">Place this class in the parent container of the footer where its associated contents are also included. You may combine this class with <code>.header-area</code> class if there's also a header incorporated.</td>
+        <td colspan="2"><code>footer</code></td>
+    </tr>
+    <tr>
+        <th align="left" colspan="5"><a href="#options" name="options">Options</a></th>
+    </tr>
+	<tr>
+		<th align="left" width="20%">Option</th>
+		<th align="left" width="40%">Description</th>
+		<th align="left" width="10%">Prerequisite</th>
+		<th align="left" width="10%">Type</th>
+		<th align="left" width="10%">Default</th>
+	</tr>
+    <tr>
+        <td><code>buttonClass</code></td>
+        <td>Allows you to add more classnames to the print button. For multiple class, add space as delimiter just like inputting classes in HTML.</td>
+        <td><code>buttonContainer</code></td>
+        <td><code>string</code></td>
+        <td><code>null</code></td>
+    </tr>
+    <tr>
+        <td><code>buttonContainer</code></td>
+        <td>A required option if you wanted to place a print button. Pass it with node name, id (<code>#id</code>) of the element or class (<code>.class</code>) of the element/s if you like to put the print button in multiple places of your page.</td>
+        <td></td>
+        <td><code>string</code></td>
+        <td><code>null</code></td>
+    </tr>
+    <tr>
+        <td><code>buttonText</code></td>
+        <td>Allows you to change the default text of the print button. You may also add the size name by adding <code>&lt;size&gt;</code> in the string. This option is recommended if you have multiple page sizes.</td>
+        <td><code>buttonContainer</code></td>
+        <td><code>string</code></td>
+        <td><code>'Print'</code></td>
+    </tr>
+    <tr>
+        <td><code>container</code></td>
+        <td>Assigns a container element to be the only content to be shown on print. For multiple containers, add comma as delimiter. Please be sure to include selector.</td>
+        <td></td>
+        <td><code>string</code></td>
+        <td><code>'body'</code></td>
+    </tr>
+    <tr>
+        <td><code>cssContainer</code></td>
+        <td>Specifies the container where the dynamically added css file will be placed. The default would be in the first-of-type <code>head</code> of the document. If there's no <code>cssPath</code> but you placed this option, it will occur an error.</td>
+        <td><code>cssPath</code></td>
+        <td><code>string</code></td>
+        <td><code>'head'</code></td>
+    </tr>
+    <tr>
+        <td><code>cssPath</code></td>
+        <td>This is used for dynamically adding of the css file of jTrintby passing its file or url path.</td>
+        <td></td>
+        <td><code>string</code></td>
+        <td><code>null</code></td>
+    </tr>
+    <tr>
+        <td><code>footer</code></td>
+        <td>Assigns a container element to be the footer of the page on print. There should be one for each <code>.footer-area</code>.</td>
+        <td><code>.footer-area</code></td>
+        <td><code>string</code></td>
+        <td><code>null</code></td>
+    </tr>
+    <tr>
+        <td><code>header</code></td>
+        <td>Assigns a container element to be the header of the page on print. There should be one for each <code>.header-area</code>.</td>
+        <td><code>.header-area</code></td>
+        <td><code>string</code></td>
+        <td><code>null</code></td>
+    </tr>
+    <tr>
+        <td><code>keypress</code></td>
+        <td>If true, it will allow you to print using <kbd>ctrl</kbd> and <kbd>P</kbd> with the configured layout.</td>
+        <td></td>
+        <td><code>boolean</code></td>
+        <td><code>false</code></td>
+    </tr>
+    <tr>
+        <td><code>margin</code></td>
+        <td>Specifies the margin for all pages. Pass margin the same way in css. There are still no function where you can change margin on multiple pages.</td>
+        <td></td>
+        <td><code>string</code></td>
+        <td><code>'0'</code></td>
+    </tr>
+    <tr>
+        <td><code>orientation</code></td>
+        <td>Allows you to change the layout of the pages. There are only two choices for this option: portrait and landscape.</td>
+        <td></td>
+        <td><code>string</code></td>
+        <td><code>'portrait'</code></td>
+    </tr>
+    <tr>
+        <td><code>size</code></td>
+        <td>Allows you to change the paper size and add more wih delimiter comma. If the desired size is not in the list, you may add manual size e.g. <code>120cmX130cm</code> in widthXheight format. The measurements should be in lowercase and the delimiter 'X' should be in uppercase.</td>
+        <td></td>
+        <td><code>string</code></td>
+        <td><code>'letter'</code></td>
+    </tr>
+</table>
 
 ## Running the Sample Website
 
-This repository contains a sample website to give demo to user who will use the Print Responsively plugin. Other files, used framework and plugins in the demo are not supposed to be requirements or prerequisites for Print Responsvely plugin to run.
-
-The demo site has documentation inside.
-
-### Prerequisites
-
-* NPM
-* Yarn
+This repository contains a sample website to give demo to user who will use the jTrint plugin. Other files, used framework and plugins in the demo are not required this plugin to run.
 
 ## Issues, Feedbacks and Suggestions
 
@@ -46,15 +153,15 @@ You're free to email me at yumitakuma@outlook.com.
 
 ## Contributing
 
-Please read [CONTRIBUTING.md](https://github.com/yumicchi/Print-Responsively/blob/master/CONTRUBUTING.md) for details on code of conduct, and the process for submitting pull requests.
+Please read [CONTRIBUTING.md](https://github.com/takumayumirin/jTrint/blob/master/CONTRUBUTING.md) for details on code of conduct, and the process for submitting pull requests.
 
 ## Versioning
 
-Please read [CHANGELOG.md](https://github.com/yumicchi/Print-Responsively/blob/master/CHANGELOG.md) for versioning.
+* Initial version 1.0
 
 ## Authors
 
-* **Yumi Takuma** - [yumicchi](https://github.com/yumicchi)
+* **Yumi Takuma** - [takumayumirin](https://github.com/takumayumirin)
 
 ## MIT License
 
---
+++ src/jtrint.js
@@ -0,0 +1,262 @@
+/************************************************
+ *
+ * Copyright (C) Yumi Takuma
+ * yumitakuma@gmail.com
+ *
+ * Date: 2019-07-08
+ *
+ ************************************************/
+
+// List of known paper sizes in mm
+var papers = [
+    { paper: '4A0', width: 1682, height: 2378 },
+    { paper: '2A0', width: 1189, height: 1682 },
+    { paper: 'A0', width: 841, height: 1189 },
+    { paper: 'A1', width: 594, height: 841 },
+    { paper: 'A2', width: 420, height: 594 },
+    { paper: 'A3', width: 297, height: 420 },
+    { paper: 'A4', width: 210, height: 297 },
+    { paper: 'A5', width: 148, height: 210 },
+    { paper: 'A6', width: 105, height: 148 },
+    { paper: 'A7', width: 74, height: 105 },
+    { paper: 'A8', width: 52, height: 74 },
+    { paper: 'A8', width: 37, height: 52 },
+    { paper: 'A10', width: 26, height: 37 },
+]
+
+_checkClass = function (element) {
+    if (element.classList.length > 0) {
+        return ' ';
+    } else {
+        return '';
+    }
+}
+
+_printPage = function (container, id, margin, orientation) {
+    var links = document.getElementsByClassName('dynamic-stylesheet-1');
+
+    for (var e = 0; e < links.length; e++) {
+        links[e].removeAttribute('disabled');
+    }
+
+    window.onbeforeprint = (event) => {
+        // Set @page
+        var stylesheet              = document.createElement('style');
+            stylesheet.rel          = 'stylesheet';
+            stylesheet.type         = 'text/css';
+            stylesheet.id           = 'dynamic-stylesheet';
+            stylesheet.innerHTML    = '@page { size: ' + id  + '; orientation: ' + orientation + '; padding-bottom: 100px; } @media print { ' + container + ' { padding: ' + margin + ' !important; } }';
+    
+        document.querySelector('head').appendChild(stylesheet);
+    };
+
+    window.print();
+
+    window.onafterprint = (event) => {
+        if (document.getElementById('dynamic-stylesheet-2'))
+            document.getElementById('dynamic-stylesheet-2').remove();
+    };
+}
+
+_setButton = function (buttonClass, buttonContainer, buttonText, container, margin, orientation, size) {
+    var buttons     = document.createElement('div'),
+        buttonText  = (buttonText) ? buttonText : 'PRINT';
+        buttons.id  = 'print-button-container';
+
+    if (buttonText.includes('<size>') && size.includes(',')) {
+        size.replace(/\s/g, '').split(',').forEach(function (obj) {
+            var button              = document.createElement('button');
+                button.type         = 'button';
+                button.className    = 'print-button' + ((buttonClass) ? ' ' + buttonClass : '');
+                button.id           = 'print-it-in-' + obj;
+                button.innerHTML    = buttonText.replace('<size>', obj);
+            
+            button.addEventListener('click', (event) => { 
+                _printPage(container, obj, margin, orientation);
+            });
+            buttons.appendChild(button);
+        });
+    } else {
+        var button              = document.createElement('button');
+            button.type         = 'button';
+            button.className    = 'print-button' + ((buttonClass) ? ' ' + buttonClass : '');
+            button.id           = 'print-it-in-' + size;
+            button.innerHTML    = (buttonText.includes('<size>')) ? buttonText + ' ' + size : buttonText;
+
+        button.addEventListener('click', (event) => {
+            _printPage(container, obj, margin, orientation);
+        });
+        buttons.appendChild(button);
+    }
+
+    if (buttonContainer.charAt(0) == '#') {
+        document.querySelector(buttonContainer).appendChild(buttons);
+    } else {
+        var classElement = document.getElementsByClassName(buttonContainer.replace('.', ''));
+
+        for (var d = 0; d < classElement.length; d++) {
+            classElement[d].prepend(buttons);
+        }
+    }
+}
+
+_setContainer = function (container) {
+    if (container.charAt(0) == '#') {
+        document.querySelector(container).className += _checkClass(container) + 'main-container-for-print';
+    } else {
+        var classElement = document.getElementsByClassName(container.replace('.', ''));
+
+        for (var a = 0; a < classElement.length; a++) {
+            classElement[a].className += _checkClass(classElement[a]) + 'main-container-for-print';
+        }
+    }
+
+    var mainContainer = document.getElementsByClassName('main-container-for-print');
+
+    for (var b = 0; b < mainContainer.length; b++) {
+        var parent = mainContainer[b];
+
+        while (parent) {
+            if (!parent.classList.contains('container-for-print') && !parent.classList.contains('main-container-for-print'))
+                parent.className += _checkClass(parent) + 'container-for-print';
+
+            if (parent.nodeName == 'HTML') {
+                break;
+            } else {
+                if (parent.childElementCount > 1) {
+                    for (var c = 0; c < parent.children.length; c++) {
+                        if (!parent.children[c].classList.contains('container-for-print') &&
+                            !parent.children[c].classList.contains('main-container-for-print') &&
+                            parent.children[c].nodeName != 'HEAD')
+                            parent.children[c].className += _checkClass(parent.children[c]) + 'hide-on-print';
+                    }
+                }
+
+                parent = parent.parentNode;
+            }
+        }
+    }
+}
+
+_setCss = function (cssContainer, cssPath) {
+    var link            = document.createElement('link');
+        link.href       = cssPath;
+        link.className  = 'dynamic-stylesheet-1'
+        link.rel        = 'stylesheet';
+        link.type       = 'text/css';
+
+    if (cssContainer.charAt(0) == '#') {
+        document.querySelector(cssContainer).appendChild(link);
+    } else {
+        var classElement = document.getElementsByClassName(cssContainer.replace('.', ''));
+
+        for (var d = 0; d < classElement.length; d++) {
+            classElement[d].prepend(link);
+        }
+    }
+}
+
+_setKeypress = function () {
+    window.addEventListener("keydown", (event) => {
+        if (event.ctrlKey || event.keyCode === 80) {
+            var links = document.getElementsByClassName('dynamic-stylesheet-1');
+
+            for (var z = 0; z < links.length; z++) {
+                links[z].setAttribute('disabled', true);
+            }
+        }
+    });
+}
+
+_printResponsively = function (param) {
+    var buttonClass     = (param.buttonClass) ? param.buttonClass : '',
+        buttonContainer = (param.buttonContainer) ? param.buttonContainer : '',
+        buttonText      = (param.buttonText) ? param.buttonText : '',
+        container       = (param.container) ? param.container : 'body',
+        cssPath         = (param.cssPath) ? param.cssPath : '',
+        cssContainer    = (param.cssContainer) ? param.cssContainer : 'head',
+        footer          = (param.footer) ? param.footer : '',
+        header          = (param.header) ? param.header : true,
+        keypress        = (param.keypress) ? param.keypress : '',
+        margin          = (param.margin) ? param.margin : '0',
+        orientation     = (param.orientation) ? param.orientation : 'portrait',
+        size            = (param.size) ? param.size : 'letter';
+
+    // Add button and set _printPage function on each
+    if (!buttonContain && buttonText) {
+        console.log('You must add the id or class of button container/s.');
+    } else if (buttonContainer) {
+        _setButton(buttonClass, buttonContainer, buttonText, container, margin, orientation, size);
+    }
+
+    // Hide all contents outside the container
+    if (container != 'body') _setContainer(container);
+
+    // Dynamically import css and place it in specific area
+    if (cssContainer && !cssPath) {
+        console.log('You must add the path to printResponsively.css via cssPath.');
+    } else if (cssPath) {
+        _setCss(cssContainer, cssPath);
+    }
+
+    // Listen to keypress
+    if (keypress == false) _setKeypress();
+
+    // SAMPLE
+    var hidBlob                     = document.createElement('div');
+        hidBlob.id                  = 'blob';
+        hidBlob.style.visibility    = 'hidden';
+        hidBlob.style.position      = 'absolute';
+        hidBlob.style.zIndex        = '-1';
+        hidBlob.style.width         = '200px';
+
+    document.body.appendChild(hidBlob);
+
+    var height = 500, width = '100px';
+    var con         = document.querySelector(container),
+        blob        = document.getElementById('blob'),
+        blobHeight  = blob.offsetHeight,
+        initHeight  = 0,
+        layers      = con.childNodes;
+
+    for (var i = 0; i < layers.length; i++) {
+        var layer = layers[i].childNodes;
+
+        // while () {
+        // }
+    }
+    if (con.offsetHeight > height) {
+        for (var i = 0; i < layers.length; i++) {
+            initHeight += (layers[i].offsetHeight == undefined) ? 0 : layers[i].offsetHeight;
+
+            if (initHeight > 500) {
+                layers[i].style.pageBreakBefore = 'always';
+                initHeight = 0;
+            }
+        }
+    }
+
+    // Get width and height of the paper
+    // var width  = 0, height   = 0;
+
+    // papers.forEach(function (item, index, array) {
+    //     if (size.toUpperCase() == item.paper) {
+    //         width   = item.width + 'mm';
+    //         height  = item.height + 'mm';
+    //     } else {
+    //         width   = size.split(' x ')[0]                                  ;
+    //         height  = size.split(' x ')[1];
+    //     }
+    // });
+};
+
+printResponsively = function (param) {
+    // console.log(param);
+    if (param) {
+        return new _printResponsively(param);
+    } else {
+        return new _printResponsively({
+            size: 'letter'
+        });
+    }
+};
